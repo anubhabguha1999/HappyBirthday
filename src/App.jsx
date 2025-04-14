00fce5f59a573a6076d77fac1853499fc53a7d72
anubhabguha1999 <anubhabguha1999@gmai.com>
2025-04-14 14:35:27 +0530

    Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -67,6 +67,7 @@ function BirthdayPage() {
     setShowPopup(true);
     setTimeout(() => {
       window.location.href = "https://ashmitabanerjee.netlify.app/";
+      setShowPopup(false);
     }, 2000);
   };
 
